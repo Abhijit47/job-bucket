e62f6c8410610d6cda678fb759929507ac0b3890
Abhijit47 <akarmakar846@gmail.com>
2025-12-23 21:57:25 +0530

    feat: add Databuddy component for enhanced analytics tracking

--- components/shared/analytics.tsx
+++ components/shared/analytics.tsx
@@ -1,11 +1,26 @@
 'use client';
 
 import UmamiAnalytics from '@danielgtmn/umami-react';
+import { Databuddy } from '@databuddy/sdk/react';
+
 const isDev = process.env.NODE_ENV === 'development';
+
 export default function Analytics() {
   return (
     <>
       <UmamiAnalytics debug={isDev} />
+
+      <Databuddy
+        clientId={process.env.NEXT_PUBLIC_DATABUDDY_CLIENT_ID}
+        trackWebVitals={true}
+        trackPerformance={true}
+        trackAttributes={true}
+        trackErrors={true}
+        trackScrollDepth={true}
+        trackHashChanges={true}
+        trackInteractions={true}
+        trackOutgoingLinks={true}
+      />
     </>
   );
 }
